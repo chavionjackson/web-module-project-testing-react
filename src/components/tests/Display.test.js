0bb9ab3901fa62ddf584a69efeac754c998c9b02
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-09-01 18:48:43 -0700

    MVP

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,16 +1,59 @@
+import React from "react";
+import { screen, render, waitFor, queryByTestId } from "@testing-library/react";
+import Display from "../Display";
+import userEvent from "@testing-library/user-event";
 
+const testDisplay = {
+  name: "Chevy's Show!",
+  seasons: [
+    { id: 0, name: "Season 1", episodes: [] },
+    { id: 1, name: "Season 2", episodes: [] },
+    { id: 2, name: "Season 3", episodes: [] },
+    { id: 3, name: "Season 4", episodes: [] },
+    { id: 4, name: "Season 5", episodes: [] },
+    { id: 5, name: "Season 6", episodes: [] },
+  ],
+  summary: "Greatest show on the planet",
+};
 
+test("the Display component renders without any passed in props", () => {
+  render(<Display />);
+});
 
+test("Test that when the fetch button is pressed, the show component will display", async () => {
+  render(<Display display={testDisplay} />);
+  const button = screen.queryByRole("button");
 
+  userEvent.click(button);
 
+  waitFor(() => {
+    const show = queryByTestId("show-container");
+    expect(show).toBeInTheDocument();
+  });
+});
 
+test("when the fetch button is pressed, the amount of select options rendered is equal to the amount of seasons in your test data", async () => {
+  render(<Display show={testDisplay} />);
+  const button = screen.getByRole("button");
 
+  userEvent.click(button);
 
+  waitFor(() =>
+    expect(screen.getAllByTestId("season-option")).toHaveLength(
+      testDisplay.seasons.length
+    )
+  );
+});
 
+test("test if optional function is being called", () => {
+  const mockClick = jest.fn();
+  render(<Display handleClick={mockClick} />);
+  const button = screen.getByRole("button");
 
+  userEvent.click(button);
 
-
-
+  waitFor(() => expect(mockClick).toHaveBeenCalledTimes(1));
+});
 
 
 ///Tasks:
